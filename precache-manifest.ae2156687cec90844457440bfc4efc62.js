b903e255ea78d4cbc0fafff1e558d4abd372413e
Michiel François <michiel.francois@student.hogent.be>
2020-05-01 11:48:56 +0200

    Updates

---
+++ precache-manifest.ae2156687cec90844457440bfc4efc62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93996599ef8af6664ee8b44c2824a3e7",
+    "url": "/bachelorproefreact/index.html"
+  },
+  {
+    "revision": "1f0cfa3d3ce6f702be57",
+    "url": "/bachelorproefreact/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7cff261475d0fadd3a84",
+    "url": "/bachelorproefreact/static/css/main.fb03c26b.chunk.css"
+  },
+  {
+    "revision": "1f0cfa3d3ce6f702be57",
+    "url": "/bachelorproefreact/static/js/2.aec07765.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/bachelorproefreact/static/js/2.aec07765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cff261475d0fadd3a84",
+    "url": "/bachelorproefreact/static/js/main.de984faf.chunk.js"
+  },
+  {
+    "revision": "82d293cfda80b7ba27b2",
+    "url": "/bachelorproefreact/static/js/runtime-main.70746886.js"
+  }
+]);
